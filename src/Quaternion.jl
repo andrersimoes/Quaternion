b4a8a861c97c4c6b66729d99e3701afd9df307cf
Andre Rodrigues Simoes <andrersimoes@gmail.com>
2019-09-09 15:12:11 -0300

    + dcm to quaternion and helper functions.

--- src/Quaternion.jl
+++ src/Quaternion.jl
@@ -1,7 +1,7 @@
 module Quaternion
 
 export Quat
-export to_axis_angle, to_axis_angled
+export to_axis_angle, to_axis_angled, get_vector
 
 struct Quat
     w::Float64
@@ -10,12 +10,14 @@ struct Quat
     z::Float64
 end
 
-Quat( a,b,c,d, w_pos::Symbol ) = (w_pos==:w_at_end) ? Quat(d,a,b,c) : Quat(a,b,c,d)
+Quat( a,b,c,d, w_pos::Symbol ) = (w_pos==:w_at_end) ? Quat(d,a,b,c) : 
+    Quat(a,b,c,d)
 
 """
     to_axis_angle(q)
 
-Extracts the rotation angle (given in radians) and rotation vector from quaternion q
+Extracts the rotation angle (given in radians) and rotation vector from 
+quaternion q
 """
 function to_axis_angle( q::Quat )
     if( abs( q.w ) ≤ 1 ) 
@@ -39,7 +41,8 @@ end
 """
     to_axis_angled(q)
 
-Extracts the rotation angle (given in degrees) and rotation vector from quaternion q
+Extracts the rotation angle (given in degrees) and rotation vector from 
+quaternion q
 """
 function to_axis_angled( q::Quat )
     r = to_axis_angle( q )
@@ -47,7 +50,19 @@ function to_axis_angled( q::Quat )
     r
 end
 
+"""
+    get_vector(q)
+Returns an Array{Float,1} formed with i, j and k coefficients of Quaternion q. 
+It can be used to directly extract the resulting vector from a rotation 
+q * qv * q', where qv is a pure imaginary quaternion representing the input 
+vector.
+"""
+function get_vector( q::Quat )
+    [ q.x q.y q.z ]
+end
+
 include( "quat_show.jl" )
 include( "quat_operations.jl" )
+include( "mat_operations.jl" )
 
 end
---
+++ src/mat_operations.jl
@@ -0,0 +1,49 @@
+export dcm, from_dcm
+
+"""
+    dcm( sys1, sys2 )
+
+Return the Directon Cosine Matrix from coordinate system 1 to coordinate 
+system 2. Both sys1 and sys2 are 3x3 matrices. Where:
+
+    sys#[1,:] == x-axis direction
+    sys#[2,:] == y-axis direction
+    sys#[3,:] == z-axis direction
+
+"""
+function dcm( sys1::Array{Float64,2}, sys2::Array{Float64,2} )
+
+    cm = Array{Float64,2}(undef,3,3)
+    for i=1:3
+        for j=1:3
+            cm[i,j] = sum( sys1[i,:] .* sys2[j,:])
+        end
+    end
+    cm
+end
+
+"""
+    from_dcm( c )
+Return a quaternion equivalent to the Direction Cossine Matrix (DCM) c
+"""
+# vide http://sites.poli.usp.br/p/eduardo.cabral/Preliminares%20Matem%C3%A1ticos.pdf
+function from_dcm( c::Array{Float64,2} )
+
+    w = 0.5 * sqrt( c[1,1] + c[2,2] + c[3,3] + 1 )
+
+    if w != 0
+        Quat( w,
+         0.5 * sign( c[3,2] - c[2,3] ) * sqrt( +c[1,1] - c[2,2] - c[3,3] + 1 ),
+         0.5 * sign( c[1,3] - c[3,1] ) * sqrt( -c[1,1] + c[2,2] - c[3,3] + 1 ),
+         0.5 * sign( c[2,1] - c[1,2] ) * sqrt( -c[1,1] - c[2,2] + c[3,3] + 1 ) 
+        )
+    else
+        if c[2,2] == c[3,3] == -1 && c[1,1] == 1
+            Quat( 0, 1, 0, 0 )
+        elseif c[1,1] == c[3,3] == -1 && c[2,2] == 1
+            Quat( 0, 0, 1, 0 )
+        elseif c[1,1] == c[2,2] == -1 && c[3,3] == 1
+            Quat( 0, 0, 0, 1 )
+        end
+    end
+end
